0fe6d7f56187470c610c30bb164bcf77a14197f0
pp9955 <arsoncyp@gmail.com>
2025-12-26 14:35:16 +0700

    plus html

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/hello/')
-def hello_world1():
-    return 'Hi my name is someone'
+@app.route('/')
+def index():
+    return render_template('index.html')
